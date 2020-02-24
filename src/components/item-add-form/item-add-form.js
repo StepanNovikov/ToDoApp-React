bc07964c5327bb2704c55ba8b7fe9f7220e90ced
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-02-24 10:32:46 +0300

    add text to list

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,15 +2,35 @@ import React, {Component} from 'react';
 import "./item-add-form.css";
 
 class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (event) => {
+        this.setState({
+            label: event.target.value
+        })
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    }
+
     render() {
         return (
-            <div className="item-add-form">
+            <form className="item-add-form d-flex"
+                    onSubmit={this.onSubmit}>
+                <input type="text"
+                        className="form-control"
+                        onChange = {this.onLabelChange}
+                        placeholder="What needs to be done"/>
                 <button 
-                    className="btn btn-outline-secondary"
-                    onClick = {() => this.props.onItemAdded('Hello world')}>
+                    className="btn btn-outline-secondary">
                     Add Form
                 </button>
-            </div>
+            </form>
         )
     }
 }
