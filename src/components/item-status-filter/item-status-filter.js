c7e93e9dccbb3048f13de187772ea28c45b8ddc7
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-02-24 13:33:49 +0300

    all,active, done filters

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -3,31 +3,33 @@ import "./item-status-filter.css"
 
 class ItemStatusFilter extends Component {
 
+    buttons = [
+        {name: 'all', label: 'All'},
+        {name: 'active', label: 'Active'},
+        {name: 'done', label: 'done'},
+    ];
     
-
     render() {
-        
-        const { onAllItems, onActiveItems, onDoneItems } = this.props;
+        const { filter, onFilterChange } = this.props;
+        const buttons = this.buttons.map(({name, label}) => {
+            const isActive = filter === name;
+            const clazz = isActive ? 'btn-info' : 'btn-outline-secondary'
+            return (
+                <button type="button"
+                        className={`btn ${clazz}`}
+                        key={name}
+                        onClick={() => {onFilterChange(name)}}>
+                            {label}
+                        </button>
+
+            )
+        })
 
         return (
             <div className="btn-group">
-                <button type="button"
-                        className="btn btn-info"
-                        onClick={onAllItems}>
-                            All
-                </button>
-                <button type="button"
-                        className="btn btn-outline-secondary"
-                        onClick={onActiveItems}>
-                            Active
-                </button>
-                <button type="button"
-                        className="btn btn-outline-secondary"
-                        onClick={onDoneItems    }>
-                            Done
-                </button>
+                {buttons}
             </div>
-        );
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ class App extends Component {
             this.createTodoItem('Make Awesome App'),
             this.createTodoItem('Have a lunch')
         ],
-        term: ''
+        term: '',
+        filter: 'active'
     };
 
     createTodoItem(label) {
@@ -92,41 +93,6 @@ class App extends Component {
         })
     };
 
-    allItems = () => {
-        const allList = []
-        
-    }
-
-    activeItems = () => {
-        const activeArr = []
-        this.setState(({todoData})=>{
-            todoData.map((el) => {
-                if(!el.done) {
-                    activeArr.push(el)
-                }
-            })
-            return {
-                todoData: activeArr
-            }
-        })
-        
-    }
-
-    doneItems = () => {
-        const doneArr = []
-        this.setState(({todoData})=>{
-            todoData.map((el) => {
-                if(el.done) {
-                    doneArr.push(el)
-                }
-            })
-            return {
-                todoData: doneArr
-            }
-        })
-        
-    }
-
     search(items, term) {
         if(term.length === 0) {
             return items;
@@ -139,14 +105,32 @@ class App extends Component {
         });
     }
 
+    filter(items, filter) {
+        switch(filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done':
+                return items.filter((item) => item.done);
+        }
+    }
+
     onSearchChange = (term) => {
         this.setState({term});
     };
 
+    onFilterChange = (filter) => {
+        this.setState({
+            filter
+        })
+    }
+
 
     render() {
-        const { todoData, term } = this.state;
-        const visibleItems = this.search(todoData, term)
+        const { todoData, term , filter } = this.state;
+        const visibleItems = this.filter(
+            this.search(todoData, term), filter)
         const doneCount = todoData
                             .filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;                    
@@ -159,10 +143,8 @@ class App extends Component {
                 <div className="top-panel d-flex">
                     <SearchPanel
                         onSearchChange={this.onSearchChange}/>
-                    <ItemStatusFilter
-                        onAllItems = {this.allItems}
-                        onActiveItems={this.activeItems}
-                        onDoneItems = {this.doneItems}/>
+                    <ItemStatusFilter filter={filter}
+                                      onFilterChange={this.onFilterChange}/>
                 </div>
                 
                 <ToDoList 
